c0a6802cf15f726d02b67cc00485db934a5f5530
lironsel <lironseliktar@gmail.com>
2017-06-27 21:45:06 +0300

    with cookies and log out

--- app/app.js
+++ app/app.js
@@ -9,21 +9,28 @@ var myApp=angular.module('myApp', [
   'myLoginApp',
   'detailsApp',
   'myApp.version',
-    'ui.bootstrap'
+    'ui.bootstrap',
+    'LocalStorageModule'
 ]);
-myApp.config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
+myApp.config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider, localStorageServiceProvider) {
   $locationProvider.hashPrefix('!');
   $routeProvider.otherwise({redirectTo: '/products'});
+ // localStorageServiceProvider.setPrefix('music_');
 }]);
-
-myApp.controller('mainController', ['userService','$http','$location','$uibModal', function(userService,$http,$location,$uibModal) {
+myApp.controller('mainController', ['userService','$http','$location','$uibModal','localStorageService','$window', function(userService,$http,$location,$uibModal, localStorageService,$window){
     var self = this;
-    self.userEmail = userService.getUserEmail();
-}]);
+    self.userEmail = userService.userEmail;
+    self.userService = userService;
 
+    self.logOut=function() {
+        userService.logOut(localStorageService);
+        $window.alert('Bye Bye see you next time!!');
+        $location.path('/home');
+    }
+}]);
 myApp.factory('productService', function() {
     var service = {};
-    service.productCategories=["Piano","Guitars","Saxophones","Trumpets","Flutes","Drums","Violins"];
+    service.productCategories=["Pianos","Guitars","Saxophones","Trumpets","Flutes","Drums","Violins"];
     service.allCategory = function () {
         return service.productCategories;
     };
@@ -39,27 +46,53 @@ myApp.factory('productService', function() {
 
 myApp.factory('userService', ['$http', function ($http) {
     var service = {};
-    service.userEmail = "Guset"
-    service.getUserEmail = function () {
-        return service.userEmail;
+    var userInStorage =  decodeURIComponent(document.cookie);
+    if(userInStorage!="")
+    {
+        service.userEmail=userInStorage.substring(3,userInStorage.indexOf("="));
+        service.isLoggedIn = true;
+        var date = new Date();
+        var dateString = date.toString();
+        dateString = dateString.substring(0, dateString.indexOf("G"));
+        service.lastLogin = "Last Entry: "+dateString;
+        console.log("2. in storage: "+service.userEmail);
     }
-
-    service.isLoggedIn = false;
+    else
+    {
+        service.userEmail= "Guest";
+        service.isLoggedIn = false;
+        service.lastLogin = "";
+    }
     service.login = function (user) {
         return $http.post('http://localhost:3100/Login', user)
             .then(function (res) {
-                var tok = res.data;
-                service.userEmail = res.data[0];
+                var token = res.data;
                 $http.defaults.headers.common = {
-                    //   'my-Token': tok,
-                    'user': user.username
+                    'my-Token': token,
+                    'user': user.mail,
+                    'lastLogin' : user.lastLogin
                 };
+                var date = new Date();
+                var dateString = date.toString();
+                dateString = dateString.substring(0, dateString.indexOf("G"));
+                service.lastLogin = "Last Entry: "+dateString;
                 service.isLoggedIn = true;
+                service.userEmail = user.mail;
+               //  var musicMail= user.mail+"=";
+               // cookieSet(musicMail , localStorageService , user.mail, user.pass);
                 return Promise.resolve(res);
             })
             .catch(function (e) {
                 return Promise.reject(e);
             });
     };
+
+    service.logOut=function(localStorageService){
+        localStorageService.cookie.clearAll();
+        localStorageService.clearAll();
+        service.userEmail= "Guest";
+        service.isLoggedIn = false;
+        service.lastLogin = "";
+    }
     return service;
 }]);
