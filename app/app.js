4e30a5c982a611765389f3225234a13eeaab6aba
windows <alonmor89@gmail.com>
2017-06-27 11:19:22 +0300

    alon commit 11:18

--- app/app.js
+++ app/app.js
@@ -8,35 +8,48 @@ var myApp=angular.module('myApp', [
   'registrationApp',
   'myLoginApp',
   'detailsApp',
-  'myApp.version'
+  'myApp.version',
+    'ui.bootstrap'
 ]);
 myApp.config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
   $locationProvider.hashPrefix('!');
   $routeProvider.otherwise({redirectTo: '/products'});
 }]);
 
+myApp.controller('mainController', ['userService','$http','$location','$uibModal', function(userService,$http,$location,$uibModal) {
+    var self = this;
+    self.userEmail = userService.getUserEmail();
+}]);
+
 myApp.factory('productService', function() {
     var service = {};
-    function set(data) {
+    service.productCategories=["Piano","Guitars","Saxophones","Trumpets","Flutes","Drums","Violins"];
+    service.allCategory = function () {
+        return service.productCategories;
+    };
+    service.set = function(data) {
         service.productId = data;
-    }
-    function get() {
+    };
+    service.get = function() {
         return service.productId;
-    }
-    return {
-        set: set,
-        get: get
-    }
+    };
+    return service;
 });
 
 
 myApp.factory('userService', ['$http', function ($http) {
     var service = {};
+    service.userEmail = "Guset"
+    service.getUserEmail = function () {
+        return service.userEmail;
+    }
+    
     service.isLoggedIn = false;
     service.login = function (user) {
         return $http.post('http://localhost:3100/Login', user)
             .then(function (res) {
                 var tok = res.data;
+                service.userEmail = res.data[0];
                 $http.defaults.headers.common = {
                     //   'my-Token': tok,
                     'user': user.username
